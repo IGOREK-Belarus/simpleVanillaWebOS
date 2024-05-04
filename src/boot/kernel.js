03d626f00d1aef5c55e5b0198f73b3be7dffbfb1
IGOREK-Belarus <135227182+IGOREK-Belarus@users.noreply.github.com>
2024-05-04 17:07:13 +0300

    Duplicate some stuff from JSON straight to script

--- src/boot/kernel.js
+++ src/boot/kernel.js
@@ -1,3 +1,8 @@
 const kernelName = "Kernel";
 const KernelStream = "DEV";
 const kernelVersion = "1.0.0";
+
+// Firefox doesn't support, so we gonna duplicate data from os.json
+const osName = "superVanillaWebOS"
+const osStream = "DEV";
+const osVersion = "1.0.0";
