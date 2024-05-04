08405d7048ba76ab138d697df258ee6812b2c16e
IGOREK-Belarus <135227182+IGOREK-Belarus@users.noreply.github.com>
2024-05-04 17:30:34 +0300

    Init shows kernel stats

--- src/boot/kernel.js
+++ src/boot/kernel.js
@@ -10,6 +10,14 @@ const osVersion = "1.0.0";
 function init(){
     console.log("Initialization started!");
     document.body.style.background = "#000000";
+
+    let mainHeaderElement = document.createElement("h1");
+    let mainHeaderText = document.createTextNode(kernelName + " " + kernelVersion);
+    document.body.appendChild(mainHeaderElement);
+    mainHeaderElement.appendChild(mainHeaderText);
+
+    // Styles
+    document.getElementsByTagName('H1')[0].style.color = "#FFFFFF";
 }
 
 
