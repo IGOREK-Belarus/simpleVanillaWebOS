f6f0efc63e0a6432bb73d4c3be658e1e82cdb8b5
IGOREK-Belarus <135227182+IGOREK-Belarus@users.noreply.github.com>
2024-05-08 17:55:21 +0300

    Add osdata.js file

---
+++ src/boot/osdata.js
@@ -0,0 +1,5 @@
+// This file exists because Firefox can't import JSON files
+// TODO: Merge osdata.js with kernel.js or somehow import os.json
+export const loader = "";
+export const loginManager = "";
+export const userShell = "";
