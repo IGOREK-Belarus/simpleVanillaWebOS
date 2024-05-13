8069323642fe9f4c7249eec277d1131085a28c86
IGOREK-Belarus <135227182+IGOREK-Belarus@users.noreply.github.com>
2024-05-13 07:04:36 +0300

    Add folders for libraries and LibIO library

---
+++ src/lib/libio.js
@@ -0,0 +1,15 @@
+// Read
+let lastKeyPressed;
+document.addEventListener("keydown", function(value){
+    lastKeyPressed = value.key;
+});
+
+export function read(){
+    return lastKeyPressed;
+}
+
+// Print
+// export function print(value){
+//     // TODO: Make it actually work.
+//     return value;
+// }
