031447ffef14380f12c77bf08991a44b940bc549
IGOREK-Belarus <135227182+IGOREK-Belarus@users.noreply.github.com>
2024-08-07 20:10:34 +0300

    Undone uncorrectly merged branch

--- src/bin/echo.js
+++
@@ -1,6 +0,0 @@
-import * as libIO from "../lib/libio.js";
-
-export function run(string){
-    libIO.print(string);
-    return 0;
-}
--- src/boot/osdata.js
+++
@@ -1,5 +0,0 @@
-// This file exists because Firefox can't import JSON files
-// TODO: Merge osdata.js with kernel.js or somehow import os.json
-export const loader = "";
-export const loginManager = "";
-export const userShell = "";
